44c30446a3a03f89ff2c3a29303e3dcc6893ccc5
yuutetu <yuutetu@gmail.com>
2020-04-27 11:37:10 +0900

    sae

---
+++ Reversi/ViewControllerPresenter.swift
@@ -0,0 +1,11 @@
+//
+//  ViewControllerPresenter.swift
+//  Reversi
+//
+//  Created by 加賀江 優幸 on 2020/04/27.
+//  Copyright © 2020 Yuta Koshizawa. All rights reserved.
+//
+
+class ViewControllerPresenter {
+    
+}
